b9f235f75e9cf46fd738dd8b7f05d9bb70898fcb
matt <echoedinvoker@gmail.com>
2023-11-19 10:33:51 +0800

    a

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [step, setStep] = useState(1)
+  const [count, setCount] = useState(1)
+
+  const date = new Date()
+  date.setDate(date.getDay() + count)
+
+  function handleMinusStep() {
+    setStep((s) => s - 1)
+  }
+  function handlePlusStep() {
+    setStep((s) => s + 1)
+  }
+  function handleMinusCount() {
+    setCount((s) => s - step)
+  }
+  function handlePlusCount() {
+    setCount((s) => s + step)
+  }
+  function handleReset() {
+    setStep(1)
+    setCount(1)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <input type="range" min="1" max="10"
+          value={step}
+          onChange={(e) => setStep(+e.target.value)}
+        />
+        <span> {step} </span>
+      </div >
+      <div>
+        <button onClick={handleMinusCount}>-</button>
+        <input type="text"
+          value={count}
+          onChange={(e) => setCount(+e.target.value)}
+        />
+        <button onClick={handlePlusCount}>+</button>
+      </div >
+      <div>{
+        `${count === 0 ? '' : Math.abs(count)} ${count > 0 ? 'count from today' : (
+          count < 0 ? 'count ago' : 'Today'
+        )} is ${new Date(date).toDateString()}`}</div>
+      {(step !== 1 || count !== 1) &&
+        <button onClick={handleReset}>Reset</button>}
+    </>
   );
 }
 
